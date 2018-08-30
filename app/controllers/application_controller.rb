bbed5afbfc9d0ca497b26002c5d5ce31fcb83db7
mguevara717 <marissa.guevara717@gmail.com>
2018-08-30 11:16:06 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,41 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-
   end
-end
+
+  get '/posts/new' do
+    @post = Post.all
+    erb :'new'
+  end
+
+  post '/posts' do
+    @post = Post.create (
+      name: params[:name]
+      content: params[:content]
+    )
+    redirect '/cows'
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :'show'
+  end
+
+
+  get '/posts/:id/edit' do
+    erb :'edit'
+  end
+
+  get '/posts/:id/delete' do
+    Post.find(params[:id]).destroy
+    erb :'delete'
+    redirect '/posts'
+  end
+
+
+
+
+
+
+
+end#END OF CLASS APPLICATION CONTROLLER
